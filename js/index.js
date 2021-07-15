7ce445a40ab1ee48ec8afa619c6fb83a6245f43b
中辻春華・極 <nakatsujiharuka@MacBook-Pro.local>
2021-07-16 07:35:14 +0900

    リモート分

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const jpStyle = gsap.timeline({
     scrollTrigger: {
         trigger: ".about",
         start: "top 70%",
-        end: 'top 30%',
+        end: 'top 20%',
         scrub: true,
     }
 });
@@ -12,9 +12,16 @@ const jpStyle = gsap.timeline({
 const lace = gsap.timeline({
     scrollTrigger: {
         trigger: ".about",
-        start: "top 30%",
-        end: 'top top',
+        start: "top 20%",
+        end: 'center top',
         scrub: true,
+    }
+});
+
+const collePanel = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".colle",
+        start: "top center",
     }
 });
 
@@ -32,6 +39,11 @@ lace.to('.about__bg--bottom', {
     x: -10
 })
 
+collePanel.to('.colle__panel--a', {duration: .4, opacity: 1, y: -20});
+collePanel.to('.colle__panel--p', {duration: .4, opacity: 1, y: -20, });
+collePanel.to('.colle__panel--m', {duration: .4, opacity: 1, y: -20,});
+collePanel.to('.colle__panel--s', {duration: .4, opacity: 1, y: -20,});
+
 const hd = document.querySelector('.hd');
 const hdBtn = document.querySelector('.nav__contact-btn');
 
@@ -43,4 +55,32 @@ window.addEventListener('scroll', () => {
         // hd.style.backgroundColor = '#eee6de';
         hd.classList.remove('change');
     }
+});
+
+ScrollReveal().reveal('.loca__text', {
+    duration: 500,
+    viewFactor: 0.4, 
+});
+
+const locaBg = [
+    'url(../images/loca_1.jpeg)',
+    'url(../images/loca_2.jpeg)'
+]
+
+let locaBgNum = 0;
+
+window.addEventListener('DOMContentLoaded', () =>{
+    document.querySelector('.loca__bg').style.backgroundImage = locaBg[1];
+
+    setInterval(() => {
+        document.querySelector('.loca__secret-bg').style.animation = 'changeLocaBg 1s';
+        document.querySelector('.loca__bg').style.backgroundImage = locaBg[locaBgNum];
+        setTimeout(() => {
+            document.querySelector('.loca__secret-bg').style.animation = 'changeLocaBg2 1s';
+        }, 2000);
+        locaBgNum++;
+        if(locaBgNum === locaBg.length) {
+            locaBgNum = 0;
+        }
+    }, 30000);
 });
